fbabe8f5c3dc09248d7461d739897aed58c626d5
André <andregparada@gmail.com>
2024-03-26 14:23:02 -0300

    feat: added cors and rate limit

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -1,9 +1,35 @@
 import express from 'express';
+import cors from 'cors';
+
+import { limiter } from './middlewares/rateLimiter';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+app.use(limiter);
+
+const allowedOrigins = ['localhost:3005'];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+  })
+);
+
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
