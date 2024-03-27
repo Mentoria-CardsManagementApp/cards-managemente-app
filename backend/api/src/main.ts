227917e5994fdcf644b142e509c5a08a554679a7
Colledev <gabrielcolle3@hotmail.com>
2024-03-27 10:06:47 -0300

    Adding requested change in PR

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import errorHandlerGlobal from './middlewares/ErrorHandlerGlobal';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -7,6 +8,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
+
+app.use(errorHandlerGlobal);
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
