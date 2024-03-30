caafb701dadabe169e77bb9779e35be3b86a590b
mariodoncel <mario@idip.com.br>
2024-03-30 18:00:50 -0300

    fix: ts config

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import errorHandlerGlobal from './middlewares/ErrorHandlerGlobal';
-import { findUserById } from './repository/user-repository';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -11,9 +10,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(errorHandlerGlobal);
-async function test() {
-  const aa = await findUserById('1');
-}
+
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
