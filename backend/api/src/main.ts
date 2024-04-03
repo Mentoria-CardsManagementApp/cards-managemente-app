e0c8e09a888fcebdac720bbf95904b1e61636c08
André <andregparada@gmail.com>
2024-04-03 17:43:13 -0300

    feat: config gracefull shutdown wip

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -12,8 +12,7 @@ app.use(limiter);
 
 const allowedOrigins = ['http://localhost:3005', 'http://localhost:4002'];
 
-app.use(
-  cors({
+app.use(cors({
     origin: function (origin, callback) {
       if (!origin) return callback(null, true);
 
@@ -25,14 +24,49 @@ app.use(
 
       return callback(null, true);
     },
-  })
-);
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
+  }));
 
 app.use(errorHandlerGlobal);
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0
+}
+
+process.on('unhandledRejection', (reason, promise) => {
+  error(
+    `App exiting due to an unhandled promise: ${promise} and reason ${reason}`
+  )
+
+  throw reason
+})
+
+process.on('uncaughtException', (error) => {
+  error(`App exiting due to an uncaught exception: ${error}`)
+  process.exit(ExitStatus.Failure)
+})
+
+try {
+  app.get('/', (req, res) => {
+    res.send({ message: 'Hello API' });
+  });
+
+  const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+  exitSignals.map((sig) => process.on(sig, async () => {
+    try {
+      await server.close();
+      error(`App exited with success`)
+      process.exit(ExitStatus.Success)
+    } catch(error) {
+      error(`App exited with error ${error}`)
+      process.exit(ExitStatus.Failure)
+    }
+  }))
+} catch(error) {
+  //como evidenciar o erro?
+  error(`App exited with error ${error}`)
+  process.exit(ExitStatus.Failure)
+}
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
