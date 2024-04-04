3f3a325d00fbbdbec897fe92216f5992d2c5772e
mariodoncel <mario@idip.com.br>
2024-04-04 20:05:53 -0300

    fix: user repository

--- backend/api/src/repository/UserRepositoryInterface.ts
+++ backend/api/src/repository/UserRepositoryInterface.ts
@@ -2,10 +2,10 @@ import { UpdateResult } from 'kysely';
 import { NewUser, User, UserUpdate } from '../database/entities/user';
 
 export interface UserRepositoryInterface {
-  findUserById(userId: string): Promise<User>;
-  findUserByGoogleId(userId: string): Promise<User>;
-  findUserByFacebookId(userId: string): Promise<User>;
-  findUserBySpotifyId(userId: string): Promise<User>;
+  findUserById(userId: string): Promise<User | undefined>;
+  findUserByGoogleId(userId: string): Promise<User | undefined>;
+  findUserByFacebookId(userId: string): Promise<User | undefined>;
+  findUserBySpotifyId(userId: string): Promise<User | undefined>;
   createUser(user: NewUser): Promise<User>;
   updateUser(userId: string, newUser: UserUpdate): Promise<UpdateResult>;
   deleteUser(userId: string): Promise<User>;
